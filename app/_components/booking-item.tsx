5374aec293ff0e8c17ea5caf2386c9d76f7e0e3b
Carlos Tavares <cmmt08@gmail.com>
2024-09-06 12:14:06 -0300

    refactor: adjusting the booking service name

--- app/_components/booking-item.tsx
+++ app/_components/booking-item.tsx
@@ -76,7 +76,9 @@ const BookingItem = ({ booking }: BookingItemProps) => {
               >
                 {isConfirmed ? "Confirmado" : "Finalizado"}
               </Badge>
-              <h3 className="font-semibold">{booking.service.name}</h3>
+              <h3 className="text-start font-semibold">
+                {booking.service.name}
+              </h3>
 
               <div className="flex items-center gap-2">
                 <Avatar className="h-6 w-6">
